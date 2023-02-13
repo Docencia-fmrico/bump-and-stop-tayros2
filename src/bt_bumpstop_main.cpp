24b26cd5bbb9678b0226f2c35f1d51ff88a94230
AdrianCobo <adrian.cobo.mh@gmail.com>
2023-02-13 11:19:19 +0100

    frecuencia kobuki cambiada funciona correcto
    Signed-off-by: AdrianCobo <adrian.cobo.mh@gmail.com>

--- src/bt_bumpstop_main.cpp
+++ src/bt_bumpstop_main.cpp
@@ -44,7 +44,7 @@ int main(int argc, char * argv[])
 
   auto publisher_zmq = std::make_shared<BT::PublisherZMQ>(tree, 10, 1666, 1667);
 
-  rclcpp::Rate rate(10);
+  rclcpp::Rate rate(20);
 
   bool finish = false;
   while (!finish && rclcpp::ok()) {
